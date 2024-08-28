224a2749cd77bcb13502e958a0940d634f40acc9
chariskar <149198618+chariskar@users.noreply.github.com>
2024-08-28 15:16:25 +0300

    forgot a couple things

--- src/Utils/Emitter.ts
+++ src/Utils/Emitter.ts
@@ -1,3 +1,19 @@
+/*
+ * This file is part of the EmcOnlineAPI project.
+ *
+ * RTC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RTC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EmcOnlineAPI.  If not, see <http://www.gnu.org/licenses/>.
+*/
 import * as events from 'events';
 
 class emitter extends events.EventEmitter {
@@ -7,7 +23,11 @@ class emitter extends events.EventEmitter {
         this.emittedEvents.add(event.toString());
         return super.emit(event, ...args);
     }
-
+    /**
+     * @deprecated
+     * @param event 
+     * @returns 
+     */
     hasEmitted(event: string): boolean {
         return this.emittedEvents.has(event);
     }
